7537de97e2366beb3d03879515dc29312723ee97
Chu-Jiwoo <smpts00@naver.com>
2022-09-26 21:58:32 +0900

    First Commit

---
+++ src/main/java/KSUTech/demo/TechLabApplication.java
@@ -0,0 +1,13 @@
+package KSUTech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KSUTech/demo/TechLabApplicationTests.java
@@ -0,0 +1,13 @@
+package KSUTech.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
